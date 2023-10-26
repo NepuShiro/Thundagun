f7030ab21a82f562918ea891caf2f951bc70ccf9
frozenreflex <frozenreflex1@gmail.com>
2023-10-26 15:57:55 -0500

    Add comment connector

---
+++ NewConnectors/ComponentConnectors/CommentConnector.cs
@@ -0,0 +1,10 @@
+using FrooxEngine;
+
+namespace Thundagun.NewConnectors.ComponentConnectors;
+
+public class CommentConnector : ComponentConnectorSingle<Comment>
+{
+    public override void ApplyChanges()
+    {
+    }
+}
