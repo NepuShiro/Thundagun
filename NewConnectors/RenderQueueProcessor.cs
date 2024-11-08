eb6bd0db5174e884b56085289efdb6eb658a069e
Nytra <14206961+Nytra@users.noreply.github.com>
2024-11-08 21:30:04 +0000

    Use custom RenderSettings without Slot refs

--- NewConnectors/RenderQueueProcessor.cs
+++ NewConnectors/RenderQueueProcessor.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FrooxEngine;
 using UnityEngine;
 using UnityFrooxEngineRunner;
 
 namespace Thundagun.NewConnectors;
+
 public class RenderQueueProcessor : MonoBehaviour
 {
     public static EngineCompletionStatus engineCompletionStatus = new EngineCompletionStatus();
@@ -27,7 +29,25 @@ public class RenderQueueProcessor : MonoBehaviour
     public Task<byte[]> Enqueue(FrooxEngine.RenderSettings settings)
     {
         var task = new TaskCompletionSource<byte[]>();
-        var renderTask = new RenderTask(settings, task);
+        var renderTask = new RenderTask(new RenderSettings { 
+            position = settings.position,
+            rotation = settings.rotation,
+            size = settings.size,
+            textureFormat = settings.textureFormat,
+            projection = settings.projection,
+            fov = settings.fov,
+            ortographicSize = settings.ortographicSize,
+            clear = settings.clear,
+            clearColor = settings.clearColor,
+            near = settings.near,
+            far = settings.far,
+            renderObjects = settings.renderObjects.Select(s => ((SlotConnector)s.Connector)?.GeneratedGameObject).ToList(),
+            excludeObjects = settings.excludeObjects.Select(s => ((SlotConnector)s.Connector)?.GeneratedGameObject).ToList(),
+            renderPrivateUI = settings.renderPrivateUI,
+            postProcesing = settings.postProcesing,
+            screenspaceReflections = settings.screenspaceReflections,
+            customPostProcess = settings.customPostProcess,
+            }, task);
 
         lock (Tasks)
         {
---
+++ NewConnectors/RenderSettings.cs
@@ -0,0 +1,48 @@
+﻿using Elements.Assets;
+using Elements.Core;
+using FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Thundagun.NewConnectors;
+
+public class RenderSettings
+{
+	public float3 position;
+
+	public floatQ rotation;
+
+	public int2 size;
+
+	public Elements.Assets.TextureFormat textureFormat;
+
+	public CameraProjection projection;
+
+	public float fov;
+
+	public float ortographicSize;
+
+	public CameraClearMode clear;
+
+	public colorX clearColor;
+
+	public float near;
+
+	public float far;
+
+	public List<GameObject> renderObjects;
+
+	public List<GameObject> excludeObjects;
+
+	public bool renderPrivateUI;
+
+	public bool postProcesing;
+
+	public bool screenspaceReflections;
+
+	public Func<Bitmap2D, Bitmap2D> customPostProcess;
+}
